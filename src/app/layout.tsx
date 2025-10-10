2a59ecc535c39afccf6eb22301fb238e3c07281a
exxsid <cortezleoanthony5@gmail.com>
2025-10-10 14:57:07 +0800

    refactor(layout): integrate Montserrat font for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+import { Open_Sans, Montserrat } from 'next/font/google'
 import './globals.css'
 
 import { cn } from '@/lib/utils'
@@ -8,6 +8,12 @@ import { Toaster } from '@/components/ui/sonner'
 
 const openSans = Open_Sans({
   variable: '--font-open-sans',
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800'],
+})
+
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800'],
 })
@@ -24,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn('antialiased', openSans.variable)}>
+      <body className={cn('antialiased', montserrat.variable)}>
         <Providers>{children}</Providers>
         <Toaster position="top-center" richColors closeButton />
       </body>
